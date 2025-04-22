12c1c3c93c487dd50ce351937c18174e2aaecfcc
Nikhil Shrivastava <nikhil.shrivastava304@gmail.com>
2025-04-23 00:32:42 +0530

    Implement solution for 'Binary Search' problem in 'Extras Done by Me' repo - Binary Search

---
+++ Extra's Done by Me/3. Binary Search/Binary Search.cpp
@@ -0,0 +1,22 @@
+int search(vector<int> &nums, int target) {
+    int n = nums.size();
+
+    int low = 0;
+    int high = n - 1;
+
+    while(low <= high){
+        int mid = (low + high) / 2;
+
+        if(nums[mid] == target) return mid;
+        else if(nums[mid] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
+
+//Problem Link: https://www.naukri.com/code360/problems/binary-search_972
+
+/*
+Time Complexity: O(logn)
+Space Complexity: O(1)
+*/
