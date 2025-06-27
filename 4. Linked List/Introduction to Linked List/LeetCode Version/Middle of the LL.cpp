437bda5c1c1df2928c79152c4ae060f6233dcfc1
Nikhil Shrivastava <nikhil.shrivastava304@gmail.com>
2025-06-27 16:21:18 +0530

    Implement solution for 'Middle of the Linked List' in LinkedList->LeetCode Version directory

---
+++ 4. Linked List/Introduction to Linked List/LeetCode Version/Middle of the LL.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <bits/stdc++.h>
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
+
+/*
+Problem Link
+LeetCode: https://leetcode.com/problems/middle-of-the-linked-list/
+Code 360: https://www.naukri.com/code360/problems/middle-of-linked-list_973250
+*/
+
+/*
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
