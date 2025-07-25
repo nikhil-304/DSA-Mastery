79bef645e0fab7ae32ea116ab027c26f0bce7ba6
Nikhil Shrivastava <nikhil.shrivastava304@gmail.com>
2025-07-25 14:11:36 +0530

    Implement solution for 'Intersection of Two Linked Lists' problem in LinkedList->LeetCode version directory

---
+++ 4. Linked List/Introduction to Linked List/LeetCode Version/Intersection of Two Linked Lists.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA == NULL || headB == NULL) return NULL;
+
+        ListNode* temp1 = headA;
+        ListNode* temp2 = headB;
+
+        while(temp1 != temp2){
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+
+            if(temp1 == temp2) return temp1;
+
+            if(temp1 == NULL) temp1 = headB;
+            if(temp2 == NULL) temp2 = headA;
+        }
+        return temp1;
+    }
+};
+
+/*
+Problem Link:
+Leetcode: https://leetcode.com/problems/intersection-of-two-linked-lists/
+Code360: https://www.naukri.com/code360/problems/intersection-of-linked-list_630457
+*/
+
+/*
+Time Complexity: O(m+n)
+Space Complexity: O(1)
+*/
